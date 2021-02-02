60eef45b52c93d981f10b2bb0c21de17b71f3bdb
Grace <gcw.chung@gmail.com>
2021-02-02 01:08:41 -0800

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Navbar.css';
+export default function Navbar() {
+    return (
+        <div className="navbar">
+             <div className="navbar-links">
+                 <div>Home</div>
+                <div className="">Menu</div>
+                <div className="">Locations</div>
+                <div className="">About</div>
+
+                <div className="logo-container">
+                    <div className="logo-main">seapot</div>
+                    <div className="logo-sub">海中锅</div>
+                </div>
+                
+                <div className="">Franchise</div>
+                <div className="reservation">Reservation</div>
+                <div className="">More</div>
+        
+             </div>
+    
+           
+
+            {/* <div className="navbar-links">
+               
+            </div> */}
+        </div>
+    )
+}
