238e070293b29fb7237308c61770e4f31c93b240
Grace <gcw.chung@gmail.com>
2021-02-02 02:17:37 -0800

    Updated the position of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,25 @@ import './Navbar.css';
 export default function Navbar() {
     return (
         <div className="navbar">
-             <div className="navbar-links">
+           
+            <div className="navbar-links">
                  <div>Home</div>
                 <div className="">Menu</div>
                 <div className="">Locations</div>
                 <div className="">About</div>
+            </div>
 
-                <div className="logo-container">
-                    <div className="logo-main">seapot</div>
-                    <div className="logo-sub">海中锅</div>
-                </div>
-                
+            <div className="logo-container">
+                <div className="logo-main">seapot</div>
+                <div className="logo-sub">海中锅</div>
+            </div>
+
+            <div className="navbar-links">
                 <div className="">Franchise</div>
                 <div className="reservation">Reservation</div>
                 <div className="">More</div>
-        
-             </div>
-    
-           
+            </div>
 
-            {/* <div className="navbar-links">
-               
-            </div> */}
-        </div>
+            </div>
     )
 }
